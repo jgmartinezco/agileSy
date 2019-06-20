84cccd4cccda3995f9a125aef59d7c976ba391c5
= <=>
2019-06-20 17:28:20 -0500

    Se establece el fichero index.js como script principal en la aplicación

--- app.js
+++ app.js
@@ -40,23 +40,5 @@ app.use('/secure', (req, res, next) => {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//module.exports = app;
+module.exports = app;
 
-var securityController = require('./controllers/security');
-var reportsController = require('./controllers/reports');
-var kpiController = require('./controllers/kpiManagement');
-
-var port = process.env.PORT || 3000;
-
-
-app.post('/login', securityController.login);
-app.get('/secure/getUser/:email', securityController.getUser);
-app.get('/secure/getMenu/:userCode', securityController.getMenu);
-
-app.get('/secure/getReportByPeriod/:IdPeriod', reportsController.reportByPeriod);
-app.get('/secure/getReportByTeamAndPeriod/:IdPeriod/:IdTeam', reportsController.reportByTeamAndPeriod);
-
-app.get('/secure/getKPIList/:IdTeam', kpiController.getKPIList);
-app.post('/secure/registerMetrics', kpiController.registerMetrics);
-
-app.listen(port, () => console.log('server is running'));
